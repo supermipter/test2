f45832b9cab846a0b99bc1bf4d4287f4160ca561
supermipter <supermipter@gmail.com>
2021-07-30 19:01:43 +0300

    two functions

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(a,b):
+    # Use a breakpoint in the code line below to debug your script.
+    sum=a+b
+    sub=a-b
+    return sum,sub
+
+def print_bye():
+    print('Bye,Bye!')
+    
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    t=print_hi(5,6)
+    print(t[1])
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
