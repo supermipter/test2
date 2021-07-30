6413e9adbcea00db7239d5d174d84498eba4a1b9
supermipter <supermipter@gmail.com>
2021-07-30 19:11:17 +0300

    three functions

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+from third_function import privet
+
 
 def print_hi(a,b):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,12 +14,12 @@ def print_hi(a,b):
 
 def print_bye():
     print('Bye,Bye!')
-    
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     t=print_hi(5,6)
     print(t[1])
-
+    privet()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
